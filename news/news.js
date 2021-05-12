b3fe18e2549dfc625876fd1062bc158b51fea187
kietnguyen250 <nntv1507@gmail.com>
2021-05-12 09:35:35 +0700

    Blog App Server

--- news/news.js
+++ news/news.js
@@ -1,19 +1,19 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class News {
-  constructor(filename = 'news.json') {
+  constructor(filename = "news.json") {
     this.path = `./data/${filename}`;
 
     try {
-      fs.readdirSync('data');
+      fs.readdirSync("data");
     } catch (error) {
-      fs.mkdirSync('data');
+      fs.mkdirSync("data");
     }
 
     try {
       fs.accessSync(this.path);
     } catch (error) {
-      fs.writeFileSync(this.path, '[]');
+      fs.writeFileSync(this.path, "[]");
     }
   }
 
@@ -24,12 +24,12 @@ class News {
   async create(data, id, imageName) {
     const totalData = JSON.parse(await fs.promises.readFile(this.path));
     const { content } = data;
-    const desc = content.substr(0, 100) + '...';
+    const desc = content.substr(0, 100) + "...";
     totalData.push({
       ...data,
       id,
       desc,
-      thumbnail: `http://192.168.0.103:3000/${imageName}`,
+      thumbnail: `http://192.168.1.3:3000/${imageName}`,
     });
 
     await fs.promises.writeFile(this.path, JSON.stringify(totalData, null, 2));
@@ -37,28 +37,28 @@ class News {
 
   async getAll() {
     const data = JSON.parse(await fs.promises.readFile(this.path));
-    return data.filter(news => delete news.content);
+    return data.filter((news) => delete news.content);
   }
 
   async searchPosts(query) {
     try {
       const data = await this.getAll();
-      return data.filter(news =>
+      return data.filter((news) =>
         news.title.toLowerCase().includes(query.toLowerCase())
       );
     } catch (error) {
-      console.log('Error while searching post.');
+      console.log("Error while searching post.");
     }
   }
 
   async getSingle(id) {
     const data = await JSON.parse(await fs.promises.readFile(this.path));
-    return data.find(news => news.id === id);
+    return data.find((news) => news.id === id);
   }
 
   async getByCategory(category) {
     const data = await this.getAll();
-    return data.filter(news => news.category === category);
+    return data.filter((news) => news.category === category);
   }
 }
 
